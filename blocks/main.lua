6e1f768593ab49177cb75558d7bff0a4ceeed086
dashketik2 <me111111@yandex.ru>
2014-09-04 19:53:56 +0300

    change

--- blocks/main.lua
+++ blocks/main.lua
@@ -16,8 +16,10 @@ local phase = event.phase
   if "began" == phase then
   x = event.xStart   -- store x location of object
   y = event.yStart   -- store y location of object
-  round = display.newRect( x, y, 70, 70)
-  physics.addBody( round, "static", { friction=0.5, bounce=0.3, radius=20 } ) 
+  round = display.newImageRect( "circle.png", 70, 70)
+  round.x = x
+  round.y = y
+  physics.addBody( round, "static", { friction=0.5, bounce=0.3, radius=35 } ) 
  end
  return true
 end
@@ -32,7 +34,8 @@ sky.x = display.contentWidth / 2
 sky.y = 200
 
 
-physics.addBody( grass, "static", { friction=0.5, bounce=0.3 } )   
+physics.addBody( grass, "static", { friction=0.5, bounce=0.3 } )
+fraction_table = {}
 
 function new_block()	
 	
@@ -57,8 +60,21 @@ function new_block()
 	
 	block = display.newRect( xRand, -10, 10, 10 ) -- before block = display.newRect( -100, 150, 50, 50 )
 
-	block:setFillColor( color[colorRand][1]/255, color[colorRand][2]/255, color[colorRand][3]/255 ) -- before block:setFillColor( color[rand][1], color[rand][2], color[rand][3] )
+	block:setFillColor( color[colorRand][1], color[colorRand][2], color[colorRand][3] ) -- before block:setFillColor( color[rand][1], color[rand][2], color[rand][3] )
 	physics.addBody( block, { density=0.9, friction=0.3, bounce=0.3} )
+	table.insert( fraction_table, block )
+
+	--for i=1, (#fraction_table) do
+		--print( #fraction_table )
+		--print( i )
+	--	print( fraction_table[i].x )
+	--	print( fraction_table[i].y )
+	--	if ( fraction_table[i].x > display.contentWidth ) and ( fraction_table[i].y > display.contentHeight ) then -- nil value coordinate
+	--		table.remove( fraction_table, i )
+	--	end
+	--end
+
+
 end
 
 local dropCrates = timer.performWithDelay( 200, new_block, -1 ) -- infinitely -- before local dropCrates = timer.performWithDelay( 50, new_block, 100 )
