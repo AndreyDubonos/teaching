d80e4b72d4e49c8a2fe29d85db0a1b3db288c33a
dashketik2 <me111111@yandex.ru>
2014-09-28 10:00:00 +0300

    +add_item

--- scroll/cr_table.lua
+++ scroll/cr_table.lua
@@ -12,7 +12,7 @@ end
 
 function display_table(table, pic, adress)
 
-	local scrollView = widget.newScrollView
+	scrollView = widget.newScrollView
 	{
 	    top = -10,
 	    left = 0,
@@ -21,8 +21,10 @@ function display_table(table, pic, adress)
 	    --scrollWidth = 600,
 	    scrollHeight = 800,
 	    horizontalScrollDisabled = true
+    	--hideBackground = true   
 	    --listener = scrollListener
 	}
+
 
 	local background = display.newRect( 0, 0, 600, 800 )
 	background:setFillColor( 0, 0, 0 )
@@ -30,7 +32,7 @@ function display_table(table, pic, adress)
 
 	--scrollView:setScrollHeight( 100 )
 
-	
+	--last_index = 0
 	
 	-- if lehgt(pic) == length(adress)
 	for i, j in pairs(pic) do
@@ -55,6 +57,8 @@ function display_table(table, pic, adress)
 		table[i].x = 30 
 		table[i].y = (i-1)*52 +30  --EROR 30 что, умножить, делить
 		scrollView:insert(table[i])
+		table.last_index = i
+		--print( table.last_index )
 		table[i]:addEventListener( "touch", function( event)
 			    if event.phase == "began" then
 			    	os.execute("start " .. adress[i])
@@ -63,5 +67,37 @@ function display_table(table, pic, adress)
 	end
 	-- body
 	return true
+end
+
+function add_item(table, pic, adress )
+	table.last_index = table.last_index + 1
+	lable_back = display.newRect( 30, (table.last_index -1)*52 +30, 600, 50 )
+
+		local web_text = display.newText( 		{
+		    text = string.sub (adress, 8),     
+		    x = 170,
+		    y = (table.last_index-1)*52 + 30,
+		    --width = 500,     --required for multi-line and alignment
+		    font = native.systemFontBold,   
+		    fontSize = 20,
+		    --align = "center"  --new alignment parameter
+		} )
+		web_text:setFillColor( 0, 0, 0 )
+		lable_back:setFillColor( math.random(50)*0.01, math.random(50)*0.01, math.random(50)*0.01 )
+		scrollView:insert(lable_back)
+		scrollView:insert(web_text)
+
+		table[table.last_index] = display.newImageRect( pic, 50, 50 )-- ERROR 5 parameters, documentation   display.newImageRect( [parentGroup,] filename, [baseDir,] width, height )
+		table[table.last_index].x = 30 
+		table[table.last_index].y = (table.last_index-1)*52 +30  --EROR 30 что, умножить, делить
+		scrollView:insert(table[table.last_index])
+		
+		
+		table[table.last_index]:addEventListener( "touch", function( event)
+			    if event.phase == "began" then
+			    	os.execute("start " .. adress)
+				end
+			end )
+	-- body
 end
 
--- scroll/main.lua
+++ scroll/main.lua
@@ -45,11 +45,16 @@ end]]
 -- Create a image and insert it into the scroll view
 logo = {"google.png", "pikabu.png", "tumblr.png", "twitter.png"}
 web = {"http://google.com", "http://pikabu.ru", "http://www.tumblr.com", "http://twitter.com"}
+item_logo = "habrhabr.png"
+item_web = "http://habrahabr.ru"
+
+my_table = create_table()
+--print( my_table[1] )
+display_table(my_table, logo, web)
+
 function handleButtonEvent( event )
     if ( "began" == event.phase ) then
-        my_table = create_table()
-        --print( my_table[1] )
-        display_table(my_table, logo, web)
+        add_item(my_table, item_logo, item_web)
         print("1")
     end
     return true
