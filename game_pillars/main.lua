fff21c690192ab9e3afcf626733185fa2f166666
dashketik2 <me111111@yandex.ru>
2015-01-13 22:13:12 +0200

    pillars_1

--- game_pillars/main.lua
+++ game_pillars/main.lua
@@ -2,31 +2,31 @@ local physics = require("physics")
 physics.start()
 
 local pillars = {}
-local x_end = 0
-local rand_y = 50
+local number_pil = 1
 
-local function new_pillar(i)
-	rand_y = math.random(200, 300)
-	pillars[i] = display.newRect(x_end, display.contentHeight, 20, rand_y)
-	pillars[i].anchorX = 0
-	pillars[i].anchorY = 1
-	pillars[i]:setFillColor(math.random(), math.random(), math.random())
-	physics.addBody(pillars[i] ,"static")
+local function new_pillar()
+	local rand_y = math.random(10, 50)
+	x_end = (number_pil-1)*20
+	pillars[number_pil] = display.newRect(x_end, display.contentHeight, 20, rand_y)
+	pillars[number_pil].anchorX = 0
+	pillars[number_pil].anchorY = 1
+	pillars[number_pil]:setFillColor(math.random(), math.random(), math.random())
+	physics.addBody(pillars[number_pil] ,"static")
 	--display.newRect( 20, 80, 280, 50 )
-	y = rand_y
 	--x_end = x_end + 20
-	x_end = i*20
-	transition.to( pillars[i], {time=math.random(10000, 15000), height = display.contentHeight} )
+
+	transition.to( pillars[number_pil], {time=math.random(10000, 15000), height = display.contentHeight / 3 * 2} )
 	print("x=", x_end)
 	print("y=", rand_y)
 	print("height", display.contentHeight - rand_y)
 	print("all=", display.contentHeight)
+	number_pil=number_pil+1
 end
 
-for i = 1, 16 do
-	new_pillar(i)
-end
+--for i = 1, 16 do
+--	new_pillar(i)
+--end
 
 
 --local show_pillars = 
---timer.performWithDelay(500, new_pillar)
+timer.performWithDelay(500, new_pillar, 25)
