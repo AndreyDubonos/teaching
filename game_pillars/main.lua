b1003a7099eb627bd2593ebff670e59bbed547c7
dashketik2 <me111111@yandex.ru>
2015-01-19 11:25:52 +0200

    +moving

--- game_pillars/main.lua
+++ game_pillars/main.lua
@@ -3,6 +3,7 @@ physics.start()
 
 local pillars = {}
 local number_pil = 1
+local pil_group = display.newGroup()
 
 local function new_pillar()
 	local rand_y = math.random(10, 50)
@@ -14,13 +15,17 @@ local function new_pillar()
 	physics.addBody(pillars[number_pil] ,"static")
 	--display.newRect( 20, 80, 280, 50 )
 	--x_end = x_end + 20
+	pil_group:insert(pillars[number_pil])
+	transition.to( pillars[number_pil], {time=math.random(10000, 15000), height = math.random(display.contentHeight / 3 * 2, display.contentHeight / 3 * 2 + 50)} )
+	--print("x=", x_end)
+	--print("y=", rand_y)
+	--print("height", display.contentHeight - rand_y)
+	--print("all=", display.contentHeight)
+	number_pil=number_pil+1 --(по надобности хранить в отдельном поле эл-та массива)
+end
 
-	transition.to( pillars[number_pil], {time=math.random(10000, 15000), height = display.contentHeight / 3 * 2} )
-	print("x=", x_end)
-	print("y=", rand_y)
-	print("height", display.contentHeight - rand_y)
-	print("all=", display.contentHeight)
-	number_pil=number_pil+1
+local function move_pillars()
+	pil_group.x = pil_group.x - 2
 end
 
 --for i = 1, 16 do
@@ -28,5 +33,5 @@ end
 --end
 
 
---local show_pillars = 
-timer.performWithDelay(500, new_pillar, 25)
+local show_pillars = timer.performWithDelay(500, new_pillar, 0)
+local move = timer.performWithDelay(60, move_pillars, 0)
